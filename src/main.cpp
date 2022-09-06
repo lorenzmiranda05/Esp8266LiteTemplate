f795b9343fbe5962c8c576189d880f1c5d56a4f3
Lorenz <lorenzmiranda05@gmail.com>
2022-09-06 22:20:18 +0800

    Connected to WiFi using ESP8266WiFiMulti.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
+#include <ESP8266WiFiMulti.h>
+
+ESP8266WiFiMulti wm;
 
 void setup()
 {
   Serial.begin(921600);
+  delay(10);
+  wm.addAP("SSID1", "Password1");
+  wm.addAP("SSID2", "Password2");
+  wm.addAP("SSID3", "Password3");
 }
 
 void loop()
 {
-}
+  if (wm.run() != WL_CONNECTED)
+  {
+    Serial.println("WiFi not connected!");
+    delay(5000);
+  }
+  if (wm.run() == WL_CONNECTED)
+  {
+    Serial.println("");
+    Serial.print("WiFi connected to ");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP Address ");
+    Serial.println(WiFi.localIP());
+    delay(5000);
+  }
